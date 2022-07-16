a78e8781148e5fec730ff0e61184c92717166311
AngelMond <abraham_will@hotmail.com>
2022-07-16 15:36:20 -0500

    Created methods to add and get data from indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,26 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: id, jate: content});
+  const result = await request;
+  console.log('Data saved to the database', result)
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+// method that gets all the content from the database
+export const getDb = async () =>{
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result;
+}
+
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -22,11 +20,15 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
+
       new MiniCssExtractPlugin(),
+      
+      // configure workbox plugins for a service worker and manifest file
       new WorkboxPlugin.GenerateSW()
     ],
 
     module: {
+      // CSS and babel loaders
       rules: [
         {
           test: /\.css$/i,
